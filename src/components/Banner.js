87947e0eae50ee105d770fe1568aaeb209b23a52
malwaredb86 <mdurham86@gmail.com>
2023-08-13 15:36:16 -0400

    Finished graded assignment 6

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import "./styling.css";
+
+export default function Banner() {
+    return (
+        <>
+            <h1>Orbit Report</h1>
+            <p>Click on the buttons to see the sattelites in that orbit type</p>
+        </>
+    )
+}
